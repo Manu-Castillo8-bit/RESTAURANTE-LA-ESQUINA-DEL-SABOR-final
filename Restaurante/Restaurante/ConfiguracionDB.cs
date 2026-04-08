734557fb6a18cfea187648e97ca450e4281b8cab
Manu-Castillo8 bits <ernesto.ccastilloguerra2@gmail.com>
2026-04-08 00:45:25 -0600

    Se creo nuestra primera aplicación

--- Restaurante/Restaurante/ConfiguracionDB.cs
+++ Restaurante/Restaurante/ConfiguracionDB.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.IO;
 using System.Windows.Forms;
 
@@ -48,44 +48,5 @@ namespace Restaurante
         }
     }
 }
-*/
 
-using System;
-using System.Deployment.Application;
-using System.IO;
-using System.Windows.Forms;
 
-namespace Restaurante
-{
-    public static class ConfiguracionDB
-    {
-        // Cadena de conexión usando |DataDirectory|
-        public static string CadenaConexion
-        {
-            get
-            {
-                return $@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\La esquina del sabor new.accdb;";
-            }
-        }
-
-        // Método para verificar si la BD existe en el DataDirectory
-        public static bool ValidarBaseDatos()
-        {
-            // Construye la ruta real de la carpeta de datos
-            string rutaDataDirectory = AppDomain.CurrentDomain.GetData("DataDirectory") as string;
-            string rutaCompletaBD = Path.Combine(rutaDataDirectory, "La esquina del sabor new.accdb");
-
-            if (!File.Exists(rutaCompletaBD))
-            {
-                MessageBox.Show(
-                    $"No se encontró la base de datos en:\n{rutaCompletaBD}\n\n" +
-                    "Asegúrate de que el archivo 'La esquina del sabor new.accdb' esté en la carpeta Data",
-                    "Error de Base de Datos",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
-                return false;
-            }
-            return true;
-        }
-    }
-}
